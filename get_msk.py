f14f613bbfeb57d38c8a836f5140aaa7564eaa72
Bodayngo <97417803+Bodayngo@users.noreply.github.com>
2023-07-17 06:54:39 -0500

    Update get_msk.py

--- get_msk.py
+++ get_msk.py
@@ -24,19 +24,13 @@ Usage:
 
 """
 
-from binascii import unhexlify, hexlify
+import argparse, re, struct
 from hashlib import md5
-import argparse, re
 
 
 def parse_arguments() -> argparse.Namespace:
     """
     Parse command-line arguments.
-
-    Description:
-        This function parses the command-line arguments necessary for decrypting the MS-MPPE-Send-Key and MS-MPPE-Recv-Key attributes
-        of RADIUS messages to derive the MSK (Master Session Key). The function expects four arguments: 'secret', 'authenticator',
-        'mppe_recv_key', and 'mppe_send_key'; all of which are required.
 
     Returns:
         args (argparse.Namespace): An object containing the parsed command-line arguments.
@@ -85,25 +79,6 @@ def decrypt_mppe_key(ciphertext: bytes, secret: bytes, authenticator: bytes, pad
     """
     Decrypts an MPPE key using the provided ciphertext, secret, authenticator, and padding.
 
-    Description:
-        The function takes an encrypted MPPE key, a shared secret, an authenticator, and an optional padding character
-        as input. It performs the decryption process and returns the decrypted MPPE key.
-
-        The decryption process involves several steps:
-            - Separating the salt and encrypted data from the input ciphertext bytes.
-            - Checking the validity of the encrypted data length, request authenticator length, and salt.
-            - Initializing an empty list to store intermediate decryption results.
-            - Constructing the initial hash input by concatenating the secret, authenticator, and salt.
-            - Iterating over the encrypted data in 16-byte blocks and performing the following steps for each encrypted block:
-                - Computing the MD5 hash of the hash input.
-                - XORing the hash result with the current encrypted block to obtain the intermediate decrypted block.
-                - Appending the intermediate decrypted block to the list.
-                - Updating the hash input by concatenating the secret and the current encrypted block.
-            - Joining the intermediate decrypted blocks to form the decrypted data.
-            - Extracting the length of the plaintext key and the actual plaintext key from the decrypted data.
-            - Performing additional checks on the length and padding of the plaintext key.
-            - Returning the plaintext key up to the specified length to remove appended padding.
-
     Args:
         ciphertext (bytes): The encrypted MPPE key.
         secret (bytes): The shared secret used in the decryption process.
@@ -117,48 +92,60 @@ def decrypt_mppe_key(ciphertext: bytes, secret: bytes, authenticator: bytes, pad
         ValueError: If the 'encrypted_data' length is invalid.
         ValueError: If the 'authenticator' length is invalid.
         ValueError: If the 'salt' is invalid.
-        ValueError: If the 'decrypted_data' is invalid.
+        ValueError: If the 'plaintext_key' is invalid.
 
     """
-    # Separate the salt and encrypted data from the input ciphertext bytes
-    salt, encrypted_data = ciphertext[0:2], ciphertext[2:]
+    BLOCK_SIZE = 16
+    MAX_ENCRYPTED_DATA_LENGTH = 256
+    SALT_LENGTH = 2
 
-    # Check the length of the encrypted data to ensure it is a multiple of 16 and not exceeding 256 bytes
-    if len(encrypted_data) % 16 or len(encrypted_data) > 256:
+    # Separate the salt and encrypted data from the input ciphertext bytes
+    salt, encrypted_data = ciphertext[:SALT_LENGTH], ciphertext[SALT_LENGTH:]
+
+    # Check the length of the encrypted data to ensure it is a multiple of BLOCK_SIZE
+    if len(encrypted_data) % BLOCK_SIZE != 0 or len(encrypted_data) > MAX_ENCRYPTED_DATA_LENGTH:
         raise ValueError("Invalid encrypted data length")
-    # Check the length of the authenticator to ensure it is a multiple of 16
-    if len(authenticator) % 16:
+
+    # Check the length of the authenticator to ensure it is a multiple of BLOCK_SIZE
+    if len(authenticator) % BLOCK_SIZE != 0:
         raise ValueError("Invalid request authenticator length")
+
     # Check the length of the salt and if the most significant bit (leftmost) is set (0x80)
-    if len(salt) != 2 or not salt[0] & 0x80:
+    if len(salt) != SALT_LENGTH or not salt[0] & 0x80:
         raise ValueError("Invalid salt")
-    
-    # Initialize an empty list to store intermediate decryption results.
+
+    # Initialize an empty list to store intermediate decryption results
     decrypted_data_blocks = []
+
     # Construct the initial hash input by concatenating the secret, authenticator, and salt
     hash_input = secret + authenticator + salt
 
-    # Iterate over the encrypted data in 16-byte blocks
-    for i in range(0, len(encrypted_data), 16):
-        encrypted_block = encrypted_data[i : i + 16]
+    # Iterate over the encrypted data in BLOCK_SIZE-byte blocks
+    for i in range(0, len(encrypted_data), BLOCK_SIZE):
+        encrypted_block = encrypted_data[i : i + BLOCK_SIZE]
         # Compute the MD5 hash of the hash input
-        hash = int(hexlify(md5(hash_input).digest()), 16)
+        hash_value = int(md5(hash_input).hexdigest(), 16)
         # XOR the hash result with the current encrypted block to obtain the intermediate decrypted block
-        decrypted_block = unhexlify("%032x" % (hash ^ int(hexlify(encrypted_block), 16)))
+        decrypted_block = bytes.fromhex("%032x" % (hash_value ^ int.from_bytes(encrypted_block, "big")))
         # Append the intermediate decrypted block to the list
         decrypted_data_blocks.append(decrypted_block)
         # Update the hash input by concatenating the secret and the current encrypted block
         hash_input = secret + encrypted_block
-    
+
     # Join the intermediate decrypted blocks to form the decrypted data
     decrypted_data = b"".join(decrypted_data_blocks)
 
     # Extract the length of the plaintext key and the actual plaintext key from the decrypted data
-    length, plaintext_key = decrypted_data[0], decrypted_data[1:]
+    length, plaintext_key = struct.unpack("!B", decrypted_data[:1])[0], decrypted_data[1:]
+
     # Check if the length is valid and the padding is correct
-    if (length > len(plaintext_key)) or (len(plaintext_key) - length > 15) or (plaintext_key[length:] != pad * (len(plaintext_key) - length)):
+    if (
+        (length > len(plaintext_key)) or 
+        (len(plaintext_key) - length > BLOCK_SIZE - 1) or 
+        (plaintext_key[length:] != pad * (len(plaintext_key) - length))
+    ):
         raise ValueError("Invalid decrypted data")
-    
+
     # Return the plaintext key up to the specified length to remove appended padding
     return plaintext_key[:length]
 
@@ -166,18 +153,6 @@ def decrypt_mppe_key(ciphertext: bytes, secret: bytes, authenticator: bytes, pad
 def main():
     """
     Main function for calculating the MSK.
-
-    Description:
-        This function serves as the entry point for calculating the MSK by decrypting the MS-MPPE-Send-Key and MS-MPPE-Recv-Key attributes of RADIUS messages.
-
-        It follows the steps below to perform the decryption and derive the MSK:
-            - Parse command-line arguments using the `parse_arguments()` function.
-            - Convert the 'secret' argument from the command line to bytes using ASCII encoding.
-            - Convert the 'authenticator', 'ms_mppe_recv_key', and 'ms_mppe_send_key' arguments from the command line to bytes using hexadecimal decoding.
-            - Decrypt the 'ms_mppe_recv_key' and 'ms_mppe_send_key' using the 'decrypt_mppe_key()' function with the 'secret' and 'authenticator'.
-            - Decode the first 32 bytes of 'decrypted_ms_mppe_recv_key' and 'decrypted_ms_mppe_send_key' as hexadecimal strings
-            - Print the MS-MPPE-Send-Key, MS-MPPE-Send-Key, and the master session key (MSK).
-                - Concatenate the MS-MPPE-Recv-Key + MS-MPPE-Send-Key to get the MSK.
 
     """
     try:
@@ -188,21 +163,22 @@ def main():
         secret = bytes(arguments.secret, 'ascii')
 
         #Convert the 'authenticator', 'ms_mppe_recv_key', and 'ms_mppe_send_key' arguments from the command line to bytes using hexadecimal decoding
-        ms_mppe_recv_key = unhexlify(arguments.ms_mppe_recv_key)
-        ms_mppe_send_key = unhexlify(arguments.ms_mppe_send_key)
-        authenticator = unhexlify(arguments.authenticator)
+        ms_mppe_recv_key = bytes.fromhex(arguments.ms_mppe_recv_key)
+        ms_mppe_send_key = bytes.fromhex(arguments.ms_mppe_send_key)
+        authenticator = bytes.fromhex(arguments.authenticator)
 
         # Decrypt the 'ms_mppe_recv_key' and 'ms_mppe_send_key' with the 'secret' and 'authenticator'
         decrypted_ms_mppe_recv_key = decrypt_mppe_key(ms_mppe_recv_key, secret, authenticator,)
         decrypted_ms_mppe_send_key = decrypt_mppe_key(ms_mppe_send_key, secret, authenticator,)
 
         # Decode the first 32 bytes of 'decrypted_ms_mppe_recv_key' and 'decrypted_ms_mppe_send_key' as hexadecimal strings
-        decoded_ms_mppe_recv_key = hexlify(decrypted_ms_mppe_recv_key[:32]).decode()
-        decoded_ms_mppe_send_key = hexlify(decrypted_ms_mppe_send_key[:32]).decode()
+        decoded_ms_mppe_recv_key = bytes.hex(decrypted_ms_mppe_recv_key[:32])
+        decoded_ms_mppe_send_key = bytes.hex(decrypted_ms_mppe_send_key[:32])
 
-        # Print the decrypted MS-MPPE-Recv-Key, decrypted MS-MPPE-Send-Key, and the master session key (MSK)
+        # Print the decrypted/decoded MS-MPPE-Recv-Key and MS-MPPE-Send-Key
         print(f"MS-MPPE-Recv-Key:    {decoded_ms_mppe_recv_key}")
         print(f"MS-MPPE-Send-Key:    {decoded_ms_mppe_send_key}")
+        # Concatenate the decrypted/decoded MS-MPPE-Recv-Key + MS-MPPE-Send-Key to get the MSK and print the result
         print(f"Master Session Key:  {decoded_ms_mppe_recv_key + decoded_ms_mppe_send_key}")
 
     except ValueError as e:
